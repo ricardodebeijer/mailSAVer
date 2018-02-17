085b4baa1f2936c6e7d862ab203c8fe7f7238088
Ricardo de Beijer <ricardodebeijer@hotmail.com>
2018-02-17 20:18:59 +0100

    Added starting project

---
+++ MailSAVerTwee/FormInstellingen.cs
@@ -0,0 +1,278 @@
+﻿using MailSaverTwee;
+using MailSAVerTwee;
+using System;
+using System.Collections.Specialized;
+using System.IO;
+using System.Windows.Forms;
+
+namespace MailSAVerAddin
+{
+    public partial class FormInstellingen : Form
+    {
+        public FormInstellingen()
+        {
+            InitializeComponent();
+        }
+
+        private void FormInstellingen_Load(object sender, EventArgs e)
+        {
+            VulInformatieVanuitSettings();
+        }
+
+        private void VulInformatieVanuitSettings()
+        {
+            VulLocaties();
+            VulNamenEmailUitgaand();
+        }
+
+        private void VulLocaties()
+        {
+            lbLocaties.Items.Clear();
+            //Standaard locatie
+            InstellingString standaard = new InstellingString(SaveManager.Instance.GetDefaultPad());
+            standaard.Standaard = true;
+            lbLocaties.Items.Add(standaard);
+            //Overige locaties oplopend
+            foreach (LocatieString locatie in SaveManager.Instance.GetLocaties())
+            {
+                InstellingString favoriet = new InstellingString(locatie);
+                lbLocaties.Items.Add(favoriet);
+            }
+            //Anders locatie
+            InstellingString anders = new InstellingString(SaveManager.Instance.GetAndersPad());
+            anders.Anders = true;
+            lbLocaties.Items.Add(anders);
+        }
+
+        private void VulNamenEmailUitgaand()
+        {
+            lbNamen.Items.Clear();
+            foreach (String naam in MailSAVerTwee.Properties.Settings.Default.EmailUitgaand)
+            {
+                lbNamen.Items.Add(naam);
+            }
+        }
+
+        private void SaveAndReload()
+        {
+            MailSAVerTwee.Properties.Settings.Default.Save();
+            MailSAVerTwee.Properties.Settings.Default.Reload();
+        }
+
+        private void btnNaamToevoegen_Click(object sender, EventArgs e)
+        {
+            if (tbNaam.Text != "")
+            {
+                if (!MailSAVerTwee.Properties.Settings.Default.EmailUitgaand.Contains(tbNaam.Text))
+                {
+                    MailSAVerTwee.Properties.Settings.Default.EmailUitgaand.Add(tbNaam.Text);
+                    SaveAndReload();
+                }
+                else
+                {
+                    MessageBox.Show("Ingevoerd persoon: '" + tbNaam.Text + "' staat al in de lijst!", "MailSAVer: Melding", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+                VulNamenEmailUitgaand();
+                tbNaam.Text = "";
+            }
+        }
+
+
+        private void btnNaamVerwijder_Click(object sender, EventArgs e)
+        {
+            if (lbNamen.SelectedItem != null)
+            {
+                MailSAVerTwee.Properties.Settings.Default.EmailUitgaand.Remove(lbNamen.SelectedItem.ToString());
+                SaveAndReload();
+                VulNamenEmailUitgaand();
+            }
+        }
+
+        private void btnLocatieVerwijder_Click(object sender, EventArgs e)
+        {
+            if (lbLocaties.SelectedItem != null)
+            {
+                InstellingString selected = (InstellingString)lbLocaties.SelectedItem;
+                MailSAVerTwee.Properties.Settings.Default.Locaties.Remove(selected.SorteringMetPad());
+                SaveAndReload();
+                VulLocaties();
+            }
+        }
+
+        private void btnZetAflopend_Click(object sender, EventArgs e)
+        {
+            if (lbLocaties.SelectedItem != null)
+            {
+                InstellingString selected = (InstellingString)lbLocaties.SelectedItem;
+                if (selected.Standaard)
+                {
+                    selected.Sortering = SORTERING.AFLOPEND;
+                    MailSAVerTwee.Properties.Settings.Default.LocatieStandaard = selected.SorteringMetPad();
+                }
+                else if (selected.Anders)
+                {
+                    selected.Sortering = SORTERING.AFLOPEND;
+                    MailSAVerTwee.Properties.Settings.Default.LocatieAnders = selected.SorteringMetPad();
+                }
+                else
+                {
+                    MailSAVerTwee.Properties.Settings.Default.Locaties.Remove(selected.SorteringMetPad());
+                    selected.Sortering = SORTERING.AFLOPEND;
+                    MailSAVerTwee.Properties.Settings.Default.Locaties.Add(selected.SorteringMetPad());
+                }
+
+                SaveAndReload();
+                VulLocaties();
+            }
+        }
+
+        private void btnZetOplopend_Click(object sender, EventArgs e)
+        {
+            if (lbLocaties.SelectedItem != null)
+            {
+                InstellingString selected = (InstellingString)lbLocaties.SelectedItem;
+                if (selected.Standaard)
+                {
+                    selected.Sortering = SORTERING.OPLOPEND;
+                    MailSAVerTwee.Properties.Settings.Default.LocatieStandaard = selected.SorteringMetPad();
+                }
+                else if (selected.Anders)
+                {
+                    selected.Sortering = SORTERING.OPLOPEND;
+                    MailSAVerTwee.Properties.Settings.Default.LocatieAnders = selected.SorteringMetPad();
+                }
+                else
+                {
+                    MailSAVerTwee.Properties.Settings.Default.Locaties.Remove(selected.SorteringMetPad());
+                    selected.Sortering = SORTERING.OPLOPEND;
+                    MailSAVerTwee.Properties.Settings.Default.Locaties.Add(selected.SorteringMetPad());
+                }
+                SaveAndReload();
+                VulLocaties();
+            }
+        }
+       
+
+        private InstellingString getObjectVanPad(string pad)
+        {
+            string orderchar = pad.Substring(0, 1);
+            if (orderchar != "+" &&
+                orderchar != "-")
+            {
+                MessageBox.Show("Fout in pad");
+            }
+            string orderremoved = pad.Substring(1);
+            InstellingString locatie = new InstellingString(orderremoved);
+            if (orderchar == "+")
+            {
+                locatie.Sortering = SORTERING.OPLOPEND;
+            }
+            else if (orderchar == "-")
+            {
+                locatie.Sortering = SORTERING.AFLOPEND;
+            }
+            return locatie;
+        }
+
+        private void VoegLocatieToe(String pad)
+        {
+            if (pad != null)
+            {
+                //default is oplopend +
+                InstellingString locatie = getObjectVanPad("+" + pad);
+                //Als het ingevoerde pad niet bestaat, melding geven
+                if (Directory.Exists(locatie.Pad))
+                {
+                    if (locatie.Pad != "")
+                    {
+                        //Als er nog geen \ achter staat, voeg die toe
+                        if (!locatie.Pad.EndsWith(@"\"))
+                        {
+                            locatie.Pad = locatie.Pad + @"\";
+                        }
+                        //Ingevoerde pad moet niet al voorkomen
+                        bool staatInLijst = false;
+
+                        //Als het pad gelijk is aan de standaard
+                        if (MailSAVerTwee.Properties.Settings.Default.LocatieStandaard == pad)
+                        {
+                            staatInLijst = true;
+                        }
+                        //Als het pad gelijk is aan de Anders
+                        else if (MailSAVerTwee.Properties.Settings.Default.LocatieAnders == pad)
+                        {
+                            staatInLijst = true;
+                        }
+                        //Als het pad gelijk is aan een van de locaties
+                        foreach (String gelijk in MailSAVerTwee.Properties.Settings.Default.Locaties)
+                        {
+                            if (gelijk == locatie.Pad)
+                            {
+                                staatInLijst = true;
+                            }
+                        }
+                        //Als hij nog niet in de lijst staat, voeg hem dan toe
+                        if (!staatInLijst)
+                        {
+                            MailSAVerTwee.Properties.Settings.Default.Locaties.Add(locatie.SorteringMetPad());
+                            SaveAndReload();
+                            VulLocaties();
+
+                        }
+                        else
+                        {
+                            MessageBox.Show("Ingevoerd pad: '" + pad + "' staat al in de lijst!", "MailSAVer: Melding", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                        }
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Ingevoerd pad: '" + pad + "' is niet correct!", "MailSAVer: Melding", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                }
+            }
+        }
+
+        private void btnFolderBrowser_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog browser = new FolderBrowserDialog();
+            browser.RootFolder = Environment.SpecialFolder.MyComputer;
+            browser.ShowDialog();
+            if (browser.SelectedPath != "")
+            {
+                VoegLocatieToe(browser.SelectedPath);
+            }
+        }
+
+        private void btnLocatieZetStandaard_Click(object sender, EventArgs e)
+        {
+            if (lbLocaties.SelectedItem != null)
+            {
+                InstellingString selected = (InstellingString)lbLocaties.SelectedItem;
+                String padoudestandaard = MailSAVerTwee.Properties.Settings.Default.LocatieStandaard;
+
+                MailSAVerTwee.Properties.Settings.Default.LocatieStandaard = selected.SorteringMetPad();
+                MailSAVerTwee.Properties.Settings.Default.Locaties.Remove(selected.SorteringMetPad());
+                VoegLocatieToe(SaveManager.Instance.getPadZonderSortering(padoudestandaard));
+                SaveAndReload();
+                VulLocaties();
+            }
+        }
+
+        private void btnLocatieZetAnders_Click(object sender, EventArgs e)
+        {
+            if (lbLocaties.SelectedItem != null)
+            {
+                InstellingString selected = (InstellingString)lbLocaties.SelectedItem;
+                String padoudeanders = MailSAVerTwee.Properties.Settings.Default.LocatieAnders;
+
+                MailSAVerTwee.Properties.Settings.Default.LocatieAnders = selected.SorteringMetPad();
+                MailSAVerTwee.Properties.Settings.Default.Locaties.Remove(selected.SorteringMetPad());
+                VoegLocatieToe(SaveManager.Instance.getPadZonderSortering(padoudeanders));
+                SaveAndReload();
+                VulLocaties();
+            }
+        }
+
+
+    }
+}
---
+++ MailSAVerTwee/LocatieString.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MailSaverTwee
+{
+   public class LocatieString : FolderString
+    {
+        public LocatieString(string pad) : base(pad)
+        {
+        }
+        public override string ToString()
+        {
+            return base.Pad;
+        }
+    }
+}
